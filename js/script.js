7b7fdbbb6322f1c18a0bfd4a23e7fbe27fbcc9e5
YanRamoss <yan.ramos@etec.sp.gov.br>
2022-09-03 08:39:08 -0300

    Att

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var instaClick = document.getElementById('insta');
 var dev = document.getElementById('dev');
+var logo = document.getElementById('logo_wrapper');
 
 var namesAndSocialMedias = ['Yan Ramos - @yanrmss\n', 'Vitor Carvalho - -/-\n', 'Daniel Alvarenga - -/-\n', 'Sofia Araújo - @sofi4_barros_\n', 'Artur Dizela - @mvp.dizela\n', 'Gustavo Belchior - @gxt_andrade\n', 'Pedro Henrique - @eu_pedro.13\n', 'Vinycius Wigner - @vinycius_wigner'];
 var devs = ['---\nYan Ramos - @yanrmss\n', 'Vitor Carvalho - -/-\n---'];
@@ -11,3 +12,15 @@ instaClick.addEventListener('click', () =>{
 dev.addEventListener('click', () =>{
     alert(devs.join(''));
 })
+
+const prefersColorScheme = window.matchMedia('(prefers-color-scheme: dark)');
+
+if( prefersColorScheme.matches ) {
+  // O tema é o dark
+  logo.innerHTML == "<img src='./img/logo_dark.png' alt='' id='logo'>";
+console.log('dark');
+} else {
+  // O tema é o light
+  logo.innerHTML == "<img src='./img/logo.png' alt='' id='logo'>"; 
+console.log('ligh');
+}
