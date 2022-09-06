65529670ce37330c2a3a544b4d2c0b3608062f3d
YanRamoss <yan.ramos@etec.sp.gov.br>
2022-09-06 20:28:50 -0300

    Att

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-const instaClick = document.getElementById('insta');
+﻿const instaClick = document.getElementById('insta');
 const dev = document.getElementById('dev');
 const logo = document.getElementById('logo_wrapper');
 const prefersColorScheme = window.matchMedia('(prefers-color-scheme: dark)');
+const colorSwitch = document.getElementById('colorSwitch');
+var a = document.getElementById('a');
 
 var namesAndSocialMedias = ['Yan Ramos - @yanrmss\n', 'Vitor Carvalho - -/-\n', 'Daniel Alvarenga - -/-\n', 'Sofia Araújo - @sofi4_barros_\n', 'Artur Dizela - @mvp.dizela\n', 'Gustavo Belchior - @gxt_andrade\n', 'Pedro Henrique - @eu_pedro.13\n', 'Vinycius Wigner - @vinycius_wigner'];
 var devs = ['---\nYan Ramos - @yanrmss\n', 'Vitor Carvalho - -/-\n---'];
@@ -15,9 +17,14 @@ dev.addEventListener('click', () =>{
 })
 
 if( prefersColorScheme.matches ) {
-  logo.innerHTML = "<img src='./img/logo_dark.png' alt='' id='logo'>";
+  // logo.innerHTML = "<img src='./img/logo_dark.png' alt='' id='logo'>";
+  logo.innerHTML = "<img src='./img/logo.png' alt='' id='logo'>"; 
 console.log('dark');
 } else {
   logo.innerHTML = "<img src='./img/logo.png' alt='' id='logo'>"; 
 console.log('ligh');
 }
+
+colorSwitch.addEventListener('click', () =>{
+  
+})
