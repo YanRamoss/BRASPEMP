eafe9951a0ba5c7c1fc73565887cbf8e77c4d584
YanRamoss <yan.ramos@etec.sp.gov.br>
2022-09-11 22:30:53 -0300

    Att

--- js/script.js
+++ js/script.js
@@ -19,7 +19,11 @@ const switcher = document.getElementById("switcher");
 window.addEventListener('load', () =>{
   var theme = window.localStorage.getItem('theme');
 if(theme == null){
+  logo.innerHTML = "<img src='./img/logo.png' alt='' id='braspemp'>";
+  figure.innerHTML = "<img src='./img/planta.png' alt='' id='planta'>";
+  switcher.innerHTML ='<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><title>Moon</title><path d="M160 136c0-30.62 4.51-61.61 16-88C99.57 81.27 48 159.32 48 248c0 119.29 96.71 216 216 216 88.68 0 166.73-51.57 200-128-26.39 11.49-57.38 16-88 16-119.29 0-216-96.71-216-216z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/></svg>';
   window.localStorage.setItem('theme', 'White');
+  console.log("light");
 } else if(theme == 'White'){
   logo.innerHTML = "<img src='./img/logo.png' alt='' id='braspemp'>";
   figure.innerHTML = "<img src='./img/planta.png' alt='' id='planta'>";
