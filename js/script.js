d8c6036a9d662f0fce271ce7b7cb600162ea69b9
YanRamoss <yan.ramos@etec.sp.gov.br>
2022-09-14 20:05:24 -0300

    Att

--- js/script.js
+++ js/script.js
@@ -24,13 +24,13 @@ if(theme == null){
 } else if(theme == 'White'){
   logo.innerHTML = "<img src='./img/logo.png' alt='' id='braspemp'>";
   figure.innerHTML = "<img src='./img/planta.png' alt='' id='planta'>";
-  switcher.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><title>Dark</title><path d="M160 136c0-30.62 4.51-61.61 16-88C99.57 81.27 48 159.32 48 248c0 119.29 96.71 216 216 216 88.68 0 166.73-51.57 200-128-26.39 11.49-57.38 16-88 16-119.29 0-216-96.71-216-216z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/></svg>';
+  switcher.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" id="icon" class="ionicon" viewBox="0 0 512 512"><title>Dark</title><path d="M160 136c0-30.62 4.51-61.61 16-88C99.57 81.27 48 159.32 48 248c0 119.29 96.71 216 216 216 88.68 0 166.73-51.57 200-128-26.39 11.49-57.38 16-88 16-119.29 0-216-96.71-216-216z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/></svg>';
   document.body.classList.remove("dark__theme");
   console.log("light");
 } else if(theme == 'Dark'){
   figure.innerHTML = "<img src='./img/planta_dark.png' alt='' id='planta'>";
   logo.innerHTML = "<img src='./img/logo_dark.png' alt='' id='braspemp'>";
-  switcher.innerHTML ='<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><title>Light</title><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="32" d="M256 48v48M256 416v48M403.08 108.92l-33.94 33.94M142.86 369.14l-33.94 33.94M464 256h-48M96 256H48M403.08 403.08l-33.94-33.94M142.86 142.86l-33.94-33.94"/><circle cx="256" cy="256" r="80" fill="none" stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="32"/></svg>';
+  switcher.innerHTML ='<svg xmlns="http://www.w3.org/2000/svg" id="icon" class="ionicon" viewBox="0 0 512 512"><title>Light</title><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="32" d="M256 48v48M256 416v48M403.08 108.92l-33.94 33.94M142.86 369.14l-33.94 33.94M464 256h-48M96 256H48M403.08 403.08l-33.94-33.94M142.86 142.86l-33.94-33.94"/><circle cx="256" cy="256" r="80" fill="none" stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="32"/></svg>';
   document.body.classList.add("dark__theme");
   console.log("dark");
 }
@@ -53,7 +53,7 @@ const alter = () => {
     figure.innerHTML = "<img src='./img/planta.png' alt='' id='planta'>";
     logo.innerHTML = "<img src='./img/logo.png' alt='' id='braspemp'>";
     switcher.innerHTML =
-      '<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><title>Dark</title><path d="M160 136c0-30.62 4.51-61.61 16-88C99.57 81.27 48 159.32 48 248c0 119.29 96.71 216 216 216 88.68 0 166.73-51.57 200-128-26.39 11.49-57.38 16-88 16-119.29 0-216-96.71-216-216z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/></svg>';
+      '<svg xmlns="http://www.w3.org/2000/svg" id="icon" class="ionicon" viewBox="0 0 512 512"><title>Dark</title><path d="M160 136c0-30.62 4.51-61.61 16-88C99.57 81.27 48 159.32 48 248c0 119.29 96.71 216 216 216 88.68 0 166.73-51.57 200-128-26.39 11.49-57.38 16-88 16-119.29 0-216-96.71-216-216z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/></svg>';
     window.localStorage.removeItem("theme");
     window.localStorage.setItem("theme", "White");
     } else if ((document.body.classList = "light__theme")) {
